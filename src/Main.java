e8fa624a876fc67a79a543be4c0ae91d03f3ae79
ZhangSiQi <1098795322@qq.com>
2021-07-05 14:18:17 +0800

    Try is again. Commit and Push on Github.

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Zhang SiQI!");
+        System.out.println("Try it Again!");
+        System.out.println("新添加一句话试一试在提交");
     }
 }
